f916ae53c6632cae5964321652ff87775d78f5f4
Marco <108359759+marcofis98@users.noreply.github.com>
2022-06-30 18:25:40 -0300

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ const meuCabecalho = document.querySelector('h1');
 meuCabecalho.textContent = '***Nostalgia Game***';
 let minhaImagem = document.querySelector('img');
 
-minhaImagem.onclick = function() {
+minhaImagem.onclick = function(){
     let meuSrc = minhaImagem.getAttribute('src');
     if(meuSrc === 'imagens/codigo.jpeg') {
       minhaImagem.setAttribute ('src','imagens/codigo2.png');
@@ -10,3 +10,34 @@ minhaImagem.onclick = function() {
       minhaImagem.setAttribute ('src','imagens/codigo.jpeg');
     }
 }
+
+
+
+let time = 1500,
+    currentImageIndex = 0,
+    images = document
+                .querySelectorAll("#slider img")
+    max = images.length;
+
+function nextImage() {
+
+    images[currentImageIndex]
+        .classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >= max)
+        currentImageIndex = 0
+
+    images[currentImageIndex]
+        .classList.add("selected")
+}
+
+function start() {
+    setInterval(() => {
+        // troca de image
+        nextImage()
+    }, time)
+}
+
+window.addEventListener("load", start)
